d600f9a81bb85aa4a7ac475f1cecb1a50bdd0c03
Abishek26S <abisheks5268@gmail.com>
2024-09-11 12:25:19 +0530

    second commit

--- vite-project/card/src/component/ComponentB.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import { ComponentC } from "./ComponentC";
-
-const ComponentB = () => {
-  return <ComponentC  />;
-};
-
-export default ComponentB;
